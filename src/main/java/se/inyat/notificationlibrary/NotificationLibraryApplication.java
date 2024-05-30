93f24020d83c5a793ee3fb93654a0bb1d6c04478
inyat <inyat.nathani@gmail.com>
2024-05-30 16:35:44 +0200

    Initial commit

---
+++ src/main/java/se/inyat/notificationlibrary/NotificationLibraryApplication.java
@@ -0,0 +1,14 @@
+package se.inyat.notificationlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationLibraryApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(NotificationLibraryApplication.class, args);
+    }
+
+}
